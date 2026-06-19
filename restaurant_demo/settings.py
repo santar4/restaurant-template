372798bf279ae685136484003ad426f2050ef190
santar4 <mdykovv@gmail.com>
2026-06-19 17:39:38 +0300

    settings

--- restaurant_demo/settings.py
+++ restaurant_demo/settings.py
@@ -1,21 +1,17 @@
-"""
-Django settings for restaurant_demo project.
-Демо-сайт ресторану для портфоліо.
-"""
+
 import os
 from pathlib import Path
 import dj_database_url
 from decouple import config
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# ВАЖЛИВО: на продакшені обов'язково зміни цей ключ через .env файл
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-CHANGE-THIS-KEY-ON-PRODUCTION-12345')
 
-# DEBUG=False на продакшені (Railway), True для локальної розробки
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']  # На реальному проєкті вкажи конкретний домен
+ALLOWED_HOSTS = ['https://restaurant-template-9ryu.onrender.com/']
 
 CSRF_TRUSTED_ORIGINS = config(
     'CSRF_TRUSTED_ORIGINS',
@@ -30,12 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menu',  # наш застосунок з меню ресторану
+    'menu',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # роздача статичних файлів на Railway
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,11 +60,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restaurant_demo.wsgi.application'
 
-# База даних: SQLite локально, PostgreSQL автоматично на Railway через DATABASE_URL
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
@@ -84,7 +80,6 @@ TIME_ZONE = 'Europe/Kyiv'
 USE_I18N = True
 USE_TZ = True
 
-# Статичні файли (CSS, JS, картинки)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -97,7 +92,7 @@ STORAGES = {
     },
 }
 
-# Медіафайли (фото страв, які завантажує клієнт через адмінку)
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
