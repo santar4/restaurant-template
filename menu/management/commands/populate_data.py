017918b10ef06a0e8e4c675b198f162a7bd39e4e
santar4 <mdykovv@gmail.com>
2026-07-23 01:19:16 +0300

    Remove Procfile, use Railway Custom Build Command

--- menu/management/commands/populate_data.py
+++ menu/management/commands/populate_data.py
@@ -4,20 +4,32 @@ from menu.models import Category, Dish
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not Category.objects.exists():
-            Category.objects.create(name='Перші страви', order=1)
-            Category.objects.create(name='Основні страви', order=2)
-            Category.objects.create(name='Десерти', order=3)
 
-            Category.objects.get(name='Перші страви').dishes.create(
-                name='Мінестроне',
-                description='Овощевий суп з пастою та пармезаном',
-                price=120.00,
-                image='media/dishes/minestrone.jpg',
-                is_available=True,
-                order=1
-            )
+        first, _ = Category.objects.get_or_create(
+            name="Перші страви",
+            defaults={"order": 1},
+        )
 
-            self.stdout.write('Дані додані!')
-        else:
-            self.stdout.write('Дані вже існують')
+        second, _ = Category.objects.get_or_create(
+            name="Основні страви",
+            defaults={"order": 2},
+        )
+
+        third, _ = Category.objects.get_or_create(
+            name="Десерти",
+            defaults={"order": 3},
+        )
+
+        Dish.objects.get_or_create(
+            name="Мінестроне",
+            defaults={
+                "category": first,
+                "description": "Овощевий суп з пастою та пармезаном",
+                "price": 120,
+                "image": "dishes/minestrone.jpg",
+                "is_available": True,
+                "order": 1,
+            },
+        )
+
+        self.stdout.write(self.style.SUCCESS("Дані успішно перевірені"))
